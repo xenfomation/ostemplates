854349a1d68d9a1a1dd00a286c253c347f6b44d7
Xenfomation <thexenfomation@gmail.com>
2015-03-05 11:05:22 -0500

    More
    More

---
+++ PardusLinux-3.0.sh
@@ -0,0 +1,31 @@
+
+##[ Pardus Linux 3.0 ]#########################################################
+# 2015, JK Benedict | @xenfomation | thexenfomation@gmail.com
+###############################################################################
+
+srcUUID=$(xe template-list name-label="Other install media" --minimal)
+dstUUID=$(xe vm-clone uuid=$srcUUID new-name-label="Pardus Linux 3.0")
+
+xe template-param-set uuid=$dstUUID \
+	name-description="COMMUNITY TEMPLATE: Pardus Linux 3.0 from http://sourceforge.net/projects/gnupardus/files/Pardus3.0/" \
+	memory-static-max=1073741824 \
+	memory-dynamic-max=1073741824 \
+	memory-dynamic-min=1073741824 \
+	other-config:linux_template=true \
+	other-config:default_template=true \
+	other-config:install-distro=debianlike \
+	other-config:install-arch=amd64 \
+	other-config:install-methods=cdrom,http,ftp,nfs \
+	other-config:disks='<provision><disk device="0" size="10737418240" sr="" bootable="true" type="system"/></provision>' \
+	platform:apic=true \
+	platform:acpi=1 \
+	platform:pae=true \
+	platform:vga=std \
+	platform:videoram=8 \
+	platform:nx=true \
+	platform:viridian=false \
+	platform:device_id=0001 \
+	HVM-boot-policy="BIOS order" \
+	HVM-boot-params:order="cdn" 
+xe template-param-remove uuid=$dstUUID param-name=other-config param-key=base_template_name
+xe template-list uuid=$dstUUID
---
+++ ScientificLinux-6.x.sh
@@ -0,0 +1,30 @@
+
+##[ Scientific Linux 6.x ]#####################################################
+# 2013-2015, JK Benedict | @xenfomation | thexenfomation@gmail.com
+###############################################################################
+
+srcUUID=$(xe template-list name-label="Other install media" --minimal)
+dstUUID=$(xe vm-clone uuid=$srcUUID new-name-label="Scientific Linux 6")
+xe template-param-set uuid=$dstUUID \
+    name-description="COMMUNITY TEMPLATE: Scientific Linux 6.x from http://ftp1.scientificlinux.org/linux/scientific/6x/" \
+    memory-static-max=1073741824 \
+    memory-dynamic-max=1073741824 \
+    memory-dynamic-min=1073741824 \
+	other-config:linux_template=true \
+	other-config:default_template=true \
+	other-config:install-distro=rhlike \
+	other-config:install-methods=cdrom,http,ftp,nfs \
+	other-config:install-arch=x86_64 \
+	other-config:disks='<provision><disk device="0" size="10737418240" sr="" bootable="true" type="system"/></provision>' \
+	platform:nx="true" \
+	platform:vga=std \
+	platform:videoram=16 \
+	platform:device_id=0001 \
+	platform:acpi=1 \
+	platform:apic=true \
+	platform:pae=true \
+	platform:viridian=true \
+	HVM-boot-policy="BIOS order" \
+	HVM-boot-params:order="cdn"
+xe template-param-remove uuid=$dstUUID param-name=other-config param-key=base_template_name
+xe template-list uuid=$dstUUID
