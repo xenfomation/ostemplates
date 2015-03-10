6ebe44448c316c3c84073ea7d5ced4b4d634e0c6
Xenfomation <thexenfomation@gmail.com>
2015-03-10 18:58:54 -0400

    Additional Templates
    Additional Templates

--- LinuxLite-2.2.sh
+++ LinuxLite-2.2.sh
@@ -1,19 +1,18 @@
 
-##[ SolydXK ]###################################################################
-# 2013-2015, JK Benedict | @xenfomation | thexenfomation@gmail.com
+##[ Linux Lite 2.2 ]############################################################
+# 2014-2015, JK Benedict | @xenfomation | thexenfomation@gmail.com
 ################################################################################
 
 srcUUID=$(xe template-list name-label="Other install media" --minimal)
-dstUUID=$(xe vm-clone uuid=$srcUUID new-name-label="SolydXK")
+dstUUID=$(xe vm-clone uuid=$srcUUID new-name-label="Linux Lite 2.2")
 
 xe template-param-set uuid=$dstUUID \
-	name-description="COMMUNITY TEMPLATE: SolydXK from http://solydxk.com/" \
+	name-description="COMMUNITY TEMPLATE: Linux Lite 2.2 from http://www.linuxliteos.com/" \
 	memory-static-max=536870912 \
 	memory-dynamic-max=536870912 \
 	memory-dynamic-min=536870912 \
 	other-config:linux_template=true \
 	other-config:install-distro=debianlike \
-	other-config:install-arch=amd64 \
 	other-config:default_template=true \
 	other-config:install-methods=cdrom,http,ftp,nfs \
 	other-config:disks='<provision><disk device="0" size="8589934592" sr="" bootable="true" type="system"/></provision>' \
