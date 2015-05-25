58e2845da5c3ed3c870a31cf191c40c7830543c2
Xenfomation <thexenfomation@gmail.com>
2015-05-25 18:34:45 -0400

    Parsix 7.5
    Adjusted RAM/VHD to minimum, recommended values

--- Parsix-7.5.sh
+++ Parsix-7.5.sh
@@ -7,14 +7,14 @@ srcUUID=$(xe template-list name-label="Other install media" --minimal)
 dstUUID=$(xe vm-clone uuid=$srcUUID new-name-label="Parsix GNU/Linux 7.5")
 xe template-param-set uuid=$dstUUID \
     name-description="COMMUNITY TEMPLATE for Parsix GNU/Linux 7.5 from http://www.parsix.org/wiki/Donate" \
-    memory-static-max=1073741824 \
-    memory-dynamic-max=1073741824 \
-    memory-dynamic-min=1073741824 \
+    memory-static-max=536870912 \
+    memory-dynamic-max=536870912 \
+    memory-dynamic-min=536870912 \
 	other-config:linux_template=true \
     other-config:default_template=true \
     other-config:install-methods=cdrom,http,ftp,nfs \
 	other-config:install-distro=debianlike \
-	other-config:disks='<provision><disk device="0" size="6442450944" sr="" bootable="true" type="system"/></provision>' \
+	other-config:disks='<provision><disk device="0" size="4294967296" sr="" bootable="true" type="system"/></provision>' \
 	platform:apic=true \
 	platform:acpi=1 \
 	platform:pae=true \
